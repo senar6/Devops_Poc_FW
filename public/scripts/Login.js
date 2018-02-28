4f70ea23f3d4aa9c6ccb59fd2d35276c2ba6d315
papunsenapati07 <papunsenapati06@gmail.com>
2018-02-28 13:44:33 +0530

    Initial Commit

---
+++ public/scripts/Login.js
@@ -0,0 +1,27 @@
+﻿
+var app = angular.module('Login', []);
+app.controller('loginCtrl', function($scope, $http) {
+    $scope.btnLoginclick = function () {
+		var url = 'http://52.160.97.191:18080/SpringMVCHibernate/authenticate'
+		var param = {};
+        param.user = $('#username').val();
+		param.password = $('#password').val();
+		var login_param = JSON.stringify(param);		//Parameter to be sent for Authentication
+		
+		$http.post(url,login_param).
+		then(function mySuccess(response) {
+		    $scope.temp = angular.fromJson(response.data);
+		    if ($scope.temp.success) {
+		        $("#validateUserPswd").hide();
+		        $("#jsonWO").val(JSON.stringify(response.data));
+		        $("#WOform").submit();
+		    }
+		    else {
+		        $("#validateUserPswd").show();
+		    }
+		    
+		}, function myError(response) {
+		    console.log(response.statusText);
+		});
+    }
+});
